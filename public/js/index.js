78c2e4a8f7f2f41f5f284860aa5522bcbdc8fa7a
evon <trananhtuan400@gmail.com>
2018-10-29 13:52:15 +0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 const socket = io();
 
+function scrollToBottom() {
+	// Selectors
+	const messages = jQuery("#messages");
+	const newMessage = messages.children("li:last-child");
+	// Heights
+	const clientHeight = messages.prop("clientHeight");
+	const scrollTop = messages.prop("scrollTop");
+	const scrollHeight = messages.prop("scrollHeight");
+	const newMessageHeight = newMessage.innerHeight();
+	const lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+
+}
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
@@ -18,7 +35,7 @@ socket.on('newMessage', function(message) {
 	});
 
 	jQuery("#messages").append(html);
-
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
 	});
 
 	jQuery("#messages").append(html);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
